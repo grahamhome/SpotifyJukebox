0f924a011e18146db8b20b36ad6539a970a8d7eb
Graham Home <grahamhome333@gmail.com>
2022-03-05 09:34:29 -0500

    Basic authentication working;gps

---
+++ flask_auth_app/project/auth.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+from flask_login import login_user, login_required, logout_user
+
+auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@auth.route("/login", methods=["POST"])
+def login_post():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    remember = bool(request.form.get("remember"))
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash(f"No user account was found for email '{email}'")
+        return redirect(url_for("auth.login"))
+    elif not check_password_hash(user.password, password):
+        flash("Please check your password and try again")
+        return redirect(url_for("auth.login"))
+    else:
+        login_user(user, remember=remember)
+        return redirect(url_for("main.profile"))
+
+
+@auth.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@auth.route("/signup", methods=["POST"])
+def signup_post():
+    email = request.form.get("email")
+    name = request.form.get("name")
+    password = request.form.get("password")
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        flash(f"A user with email address '{email}' already exists.")
+        return redirect(url_for("auth.signup"))
+
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method="sha256"))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for("auth.login"))
+
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
+
---
+++ flask_auth_app/project/models.py
@@ -0,0 +1,11 @@
+from . import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    """
+    A user of the application.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
